5a8a7b56908bc93ff3d7f47fb94d34a1d4486116
eonnembo <elias.onnembo@maxisistemas.com.ar>
2024-06-26 07:59:33 -0300

    subida

--- Clase 29/apicrud/data/db.js
+++ Clase 29/apicrud/data/db.js
@@ -2,7 +2,7 @@
 const {Sequelize} = require("sequelize")
 
 // nombre de tu base de datos, users, contraseña, donde esta alojada la db, en que lenguaje esta la db, en que puerto la db
-const db = new Sequelize("posteos24127", "root", "", {
+const db = new Sequelize("posteos", "root", "root", {
     host: "localhost",
     dialect: "mysql",
     port: 3306
